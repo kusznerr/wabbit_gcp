25a790010e4eaf1242d464ff11c2da039d5a097e
kuszner <kuszner@gmail.com>
2020-06-07 22:24:36 +0200

    IP + nomad

--- main.tf
+++ main.tf
@@ -10,9 +10,40 @@ resource "google_compute_address" "test-static-ip-address" {
  region = var.region
  }
 
-resource "google_compute_instance" "vm_instance" {
+ resource "google_compute_address" "internal_ip" {
+  count  = var.gcp_vm_count
+  name         = "${var.wabbitwww}${count.index}-internal-address"
+  address_type = "INTERNAL"
+  //network       = google_compute_network.vpc_network.self_link
+  region       = "us-central1"
+}
+
+resource "google_compute_instance" "bbb_instance" {
   count        = var.gcp_vm_count
   name         = "${var.wabbitwww}${count.index}"
+  zone        = var.zone
+  machine_type = "f1-micro"
+  //machine_type = "c2-standard-16"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network       = google_compute_network.vpc_network.self_link
+    network_ip = google_compute_address.internal_ip[count.index].address
+    access_config {
+      nat_ip = google_compute_address.test-static-ip-address[count.index].address
+    }
+  }
+}
+
+resource "google_compute_instance" "nomad_instance" {
+  //count        = var.gcp_vm_count
+  name         = "nomad-${var.wabbitwww}"
   zone        = var.zone
   machine_type = "f1-micro"
 
@@ -26,8 +57,7 @@ resource "google_compute_instance" "vm_instance" {
     # A default network is created for all GCP projects
     network       = google_compute_network.vpc_network.self_link
     access_config {
-      nat_ip = google_compute_address.test-static-ip-address[count.index].address
-    }
+      }
   }
 }
 
