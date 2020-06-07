17c688649c82dc2963b3b8e92d4858cdd88a5dd0
kuszner <kuszner@gmail.com>
2020-06-07 22:57:57 +0200

    ip range fix

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ resource "google_compute_network" "vpc_network" {
 
 resource "google_compute_subnetwork" "wabbit-subnet" {
   name    = "${var.network_name}-subnet"
-  ip_cidr_range = "10.10.10.10/22"
+  ip_cidr_range = "10.0.0.0/24  "
   region = var.region
   network = google_compute_network.vpc_network.self_link
 }
