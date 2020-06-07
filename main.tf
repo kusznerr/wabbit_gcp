6b359f56ad76b9be94d91c0c3039bd812e001ffa
kuszner <kuszner@gmail.com>
2020-06-07 23:02:56 +0200

    jeszcze raz

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ resource "google_compute_network" "vpc_network" {
 
 resource "google_compute_subnetwork" "wabbit-subnet" {
   name    = "${var.network_name}-subnet"
-  ip_cidr_range = "10.0.0.1/32"
+  ip_cidr_range = "10.2.0.0/16"
   region = var.region
   network = google_compute_network.vpc_network.self_link
 }
