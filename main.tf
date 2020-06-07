b6c93466a6f11d188c792f995fb1f0e7365fae5f
Kuszner <kuszner@gmail.com>
2020-06-07 13:20:34 +0200

    Sample instance

--- main.tf
+++ main.tf
@@ -1,7 +1,29 @@
 provider "google" {
   //credentials = "${file("account.json")}"
-  //project     = "wabbit-rk2"
-  region      = "us-central1"
-  zone        = "us-central1-c"
+  //project     = "wabbit-rk2" Removed project reference - added to terraform cloud workspace veriables
+  region      = "europe-west3"
+  zone        = "europe-west3-c"
 }
 
+resource "google_compute_instance" "vm_instance" {
+  name         = "www40"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network       = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "wabbit-network"
+  auto_create_subnetworks = "true"
+}
